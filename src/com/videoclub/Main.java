1f02a4c3248fbdfd7667dfca68f044ee913d5deb
BluewyWork <bluewywork@gmail.com>
2023-04-11 18:59:07 +0200

    stuff

--- src/com/videoclub/Main.java
+++ src/com/videoclub/Main.java
@@ -47,32 +47,32 @@ public class Main {
 
 	public static void menuOptions() {
 		System.out.println("### MENU ###");
-		System.out.println("1. Altas");
-		System.out.println("2. Alquilar Multimedia a Socio");
-		System.out.println("3. Devolver Multimedia a Socio");
-		System.out.println("4. Listar");
-		System.out.println("0. Salir");
+		System.out.println("[1] Altas");
+		System.out.println("[2] Alquilar Multimedia a Socio");
+		System.out.println("[3] Devolver Multimedia a Socio");
+		System.out.println("[4] Listar");
+		System.out.println("[0] Salir");
 		System.out.println("--- ---- ---");
 	}
 	
 	public static void altasMenuOptions() {
 		System.out.println("### Altas MENU ###");
-		System.out.println("1. Alta a Socio");
-		System.out.println("2. Alta a Pelicula");
-		System.out.println("3. Alta a Videojuego");
-		System.out.println("0. Salir");
+		System.out.println("[1] Alta a Socio");
+		System.out.println("[2] Alta a Pelicula");
+		System.out.println("[3] Alta a Videojuego");
+		System.out.println("[0] Salir");
 		System.out.println("--- ---- ---- ---");
 	}
 
 	public static void listarMenuOptions() {
 		System.out.println("### Listar MENU ###");
-		System.out.println("1. Listado de todos los objetos multimedia");
-		System.out.println("2. Listado de todas las películas ordenadas por título");
-		System.out.println("3. Listado de todas las canciones de un disco por duración");
-		System.out.println("4. Listado de todos los videojuegos ordenados por año");
-		System.out.println("5. Listado de los alquileres actuales de un socio");
-		System.out.println("6. Listado de los socios con recargos pendientes");
-		System.out.println("0. Salir");
+		System.out.println("[1] Listado de todos los objetos multimedia");
+		System.out.println("[2] Listado de todas las películas ordenadas por título");
+		System.out.println("[3] Listado de todas las canciones de un disco por duración");
+		System.out.println("[4] Listado de todos los videojuegos ordenados por año");
+		System.out.println("[5] Listado de los alquileres actuales de un socio");
+		System.out.println("[6] Listado de los socios con recargos pendientes");
+		System.out.println("[7] Salir");
 		System.out.println("--- ------ ---- ---");
 	}
 
