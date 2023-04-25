e27de1083fc4cba076f91a455a60a5f5824f2da4
dam2 <dam2>
2023-04-25 16:50:08 +0200

    attempt 1 GUI

---
+++ src/com/videoclub/view/GUI.java
@@ -0,0 +1,16 @@
+package com.videoclub.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame implements ActionListener {
+	JMenuBar menuBar;
+	JMenu menuGroup1;
+	JPanel panel;
+
+	public void ventanaPrincipal() {
+		panel = new JPanel(new GridLayout(2, 2));
+
+	}
+}
