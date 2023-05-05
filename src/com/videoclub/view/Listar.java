ec9d31f7d42b4d93bb736686cea88d5537879c85
BluewyWork <bluewywork@gmail.com>
2023-05-05 21:43:27 +0200

    stuff

---
+++ src/com/videoclub/view/Listar.java
@@ -0,0 +1,27 @@
+package com.videoclub.view;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Listar extends JPanel implements ActionListener {
+
+	JButton buttonListadoTodosObjectosMultimedia;
+	JButton buttonPeliculasOrdenadoPorTitulo;
+	JButton buttonCancionesDiscoDuracion;
+	JButton buttonVideojuegosPorAnyo;
+	JButton buttonAquileresDeSocio;
+	JButton buttonSociosRecargosPendientes;
+
+	public Listar() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+	}
+	
+}
