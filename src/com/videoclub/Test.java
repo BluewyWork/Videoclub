c16c586d53727e72fb9d19967a7b89affb6416c7
BluewyWork <bluewy.s.work@gmail.com>
2023-05-05 19:39:00 +0200

    sutff

--- src/com/videoclub/Test.java
+++ src/com/videoclub/Test.java
@@ -1,13 +1,12 @@
 package com.videoclub;
 
-import com.videoclub.lib.*;
+import com.videoclub.view.*;
 
 public class Test {
 	public static void main(String[] args) {
-		int one;
-
-		one = Input.readInput("Input: ", "int");
-
-		System.out.println(one);
-	}	
+		GUI gui = new GUI();
+		
+		gui.setVisible(true);
+		gui.setBounds(100, 100, 900, 900);
+	}
 }
---
+++ src/com/videoclub/view/Altas.java
@@ -0,0 +1,22 @@
+package com.videoclub.view;
+
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Altas extends JPanel implements ActionListener {
+
+	JButton button;
+
+	public Altas() {
+		add(button);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+	}
+}
