cacd56d8bf8a24ce08e45ffee28ced7f1fd9e664
BluewyWork <bluewy.s.work@gmail.com>
2023-05-07 18:17:52 +0200

    start

---
+++ src/com/videoclub/controller/ConsoleMenu.java
@@ -0,0 +1,12 @@
+package com.videoclub.controller;
+
+import com.videoclub.lib.Input;
+import com.videoclub.view.MenuView;
+
+public class ConsoleMenu
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
