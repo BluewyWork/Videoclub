4410ca20ede95e75be34066e4c718ed607d022d6
BluewyWork <bluewy.s.work@gmail.com>
2023-05-13 02:46:59 +0200

    eliminated blank lines

--- src/com/videoclub/model/Constants.java
+++ src/com/videoclub/model/Constants.java
@@ -5,8 +5,6 @@ public class Constants
 	public static final String BASE_PATH = "$HOME/.local/share/videoclub/";
 	public static final String FILE_FORMAT = ".ser";
 	public static final int RENT_MAX_DURATION = 3;
-
 	public static final double BASE_PRICE = 4.0;
-
 	public static final double SURCHARGE_PER_DAY = 2;
 }
