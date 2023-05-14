5b16788668833a0f390bd762af3d814a01eb7676
JIAN DAN CAI <chitancai@gmail.com>
2023-05-14 23:48:36 +0200

    Actualización de la interfaz gráfica

--- src/com/videoclub/view/MenuView.java
+++ src/com/videoclub/view/MenuView.java
@@ -1,9 +1,13 @@
 package com.videoclub.view;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class MenuView extends JFrame implements ActionListener
 {
@@ -13,17 +17,63 @@ public class MenuView extends JFrame implements ActionListener
 	private final JButton buttonAlquilarMultimediaASocio;
 	private final JButton buttonDevolverMultimedia;
 	private final JButton buttonListar;
+	private JMenuBar menuBar;
+	private JMenu menuHome, menuAlta, menuListar, menuAlquilar, menuDevolver;
+	private JMenuItem altaSocio, altaPelicula, altaVideojuego, listarMultimedia,
+			listarPelicula, listarCancion, listarVideojuego, listarAlquiler, listarSocio;
+	IntroduccionView panelIntroduccion;
 	AltasView panelAltas;
 	AltaPeliculaView panelPeliculas;
 
 	public MenuView()
 	{
+		//Aquí se crea el menuBar
+		menuBar = new JMenuBar();
+		menuHome = new JMenu("Home");
+		menuAlta = new JMenu("Altas");
+		menuAlquilar = new JMenu("Alquilar Multimedia");
+		menuDevolver = new JMenu("Devolver Multimedia");
+		altaSocio = new JMenuItem("Alta de socio");
+		altaPelicula = new JMenuItem("Alta de pelicula");
+		altaVideojuego = new JMenuItem("Alta de videojuego");
+		menuAlta.add(altaSocio);
+		menuAlta.add(altaPelicula);
+		menuAlta.add(altaVideojuego);
+		menuListar = new JMenu("Listar");
+		listarMultimedia = new JMenuItem("Todos los objetos multimedia");
+		listarPelicula = new JMenuItem("Todas las peliculas ordenados por título");
+		listarCancion = new JMenuItem("Todas las canciones de un disco por duración");
+		listarVideojuego = new JMenuItem("Todos los videojuegos ordenados por año");
+		listarAlquiler = new JMenuItem("Los alquileres actuales de un socio");
+		listarSocio = new JMenuItem("Los socios con recargos pendientes");
+		menuListar.add(listarMultimedia);
+		menuListar.add(listarPelicula);
+		menuListar.add(listarCancion);
+		menuListar.add(listarVideojuego);
+		menuListar.add(listarAlquiler);
+		menuListar.add(listarSocio);
+		menuAlquilar = new JMenu("Alquilar");
+		menuDevolver = new JMenu("Devolver");
+
+		menuBar.add(menuHome);
+		menuBar.add(menuAlta);
+		menuBar.add(menuAlquilar);
+		menuBar.add(menuDevolver);
+		menuBar.add(menuListar);
+		//Aqui termina de crear e inicializar items, menus, etc...
+
+		setJMenuBar(menuBar);
+		setTitle("JAMA Videoclub");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400, 300);
 
 		contentPane = new JPanel();
 		cardLayout = new CardLayout();
 		contentPane.setLayout(cardLayout);
+
+		panelIntroduccion = new IntroduccionView();
+		panelIntroduccion.setName("panelIntroduccion");
+		contentPane.add(panelIntroduccion, "panelIntroduccion");
 
 		panelAltas = new AltasView();
 		panelAltas.setName("panelAltas");
@@ -44,6 +94,39 @@ public class MenuView extends JFrame implements ActionListener
 		ListarView panelListarView = new ListarView();
 		panelListarView.setName("panelListarView");
 		contentPane.add(panelListarView, "panelListarView");
+
+		menuHome.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+
+				cardLayout.show(contentPane, "panelIntroduccion");
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e)
+			{
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e)
+			{
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e)
+			{
+				menuHome.setPopupMenuVisible(true);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e)
+			{
+
+			}
+		});
 
 		buttonAltas = new JButton("Altas");
 		buttonAltas.addActionListener(this);
@@ -71,9 +154,9 @@ public class MenuView extends JFrame implements ActionListener
 	{
 		String panelName = "";
 
-		if (e.getSource() == buttonAltas)
+		if (e.getSource() == menuHome)
 		{
-			panelName = "panelAltas";
+			panelName = "panelIntroduccion";
 		}
 		else if (e.getSource() == buttonAltas)
 		{
@@ -90,6 +173,10 @@ public class MenuView extends JFrame implements ActionListener
 		else if (e.getSource() == buttonListar)
 		{
 			panelName = "panelListar";
+		}
+		else if (e.getSource() == buttonAltas)
+		{
+			panelName = "panelAltas";
 		}
 
 		cardLayout.show(contentPane, panelName);
