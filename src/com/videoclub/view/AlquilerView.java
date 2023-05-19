e45c6cee3f777da9390644bfff0ecac14acafdf9
BluewyWork <bluewy.s.work@gmail.com>
2023-05-19 18:51:43 +0200

    stuff

--- src/com/videoclub/view/AlquilerView.java
+++ src/com/videoclub/view/AlquilerView.java
@@ -123,7 +123,7 @@ public class AlquilerView extends JPanel implements ActionListener
 		public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus)
 		{
 			super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-			if (value instanceof Alquiler)
+			if (value instanceof Multimedia)
 			{
 				Multimedia multimedia = (Multimedia) value;
 				setText(multimedia.getTitulo());
