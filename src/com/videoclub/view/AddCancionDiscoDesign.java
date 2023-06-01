1efd3911833153b0427a685f6816a5dcd9995e04
JIAN DAN CAI <chitancai@gmail.com>
2023-06-02 00:02:38 +0200

    Disenio de aniadir cancion a disco cambiada

--- src/com/videoclub/view/AddCancionDiscoDesign.java
+++ src/com/videoclub/view/AddCancionDiscoDesign.java
@@ -7,6 +7,7 @@ import com.videoclub.model.Constantes;
 import com.videoclub.model.Disco;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -14,113 +15,154 @@ import java.util.ArrayList;
 /**
  * Esta clase representa una interfaz de usuario para agregar canciones a un disco en un videoclub.
  */
-public class AddCancionDiscoDesign extends JFrame implements ActionListener
+public class AddCancionDiscoDesign extends JFrame
 {
+	private AniadirCancionDisco panel;
+	private CancionController cancionController;
+	private MultimediaController multimediaController;
 
 	/**
-	 * Declaración de controllers
-	 */
-	MultimediaController multimediaController;
-	CancionController cancionController;
-	/**
-	 * Declaración de atributos
-	 */
-	private JPanel panel;
-	private JLabel lblSelectDisco;
-	private JLabel lblSelectCancion;
-	private JButton btnAdd;
-	private JComboBox cboxDisco;
-	private JComboBox cboxCancion;
-	private final ArrayList<Cancion> listCanciones;
-	private ArrayList<Disco> listDiscos;
-
-	/**
-	 * Inicializa una instancia de la clase AddCancionDiscoDesign.
+	 * Crea una instancia de `AddCancionDiscoDesign`.
 	 *
-	 * @param mc el controlador de multimedia
-	 * @param cc el controlador de canciones
+	 * @param cancionController el controlador de canciones
 	 */
-	public AddCancionDiscoDesign(MultimediaController mc, CancionController cc)
+	public AddCancionDiscoDesign(MultimediaController multimediaController, CancionController cancionController)
 	{
-		multimediaController = mc;
-		cancionController = cc;
-		listCanciones = cancionController.returnCancion();
-
+		this.multimediaController = multimediaController;
+		this.cancionController = cancionController;
 		initComponents();
-		configComponents();
 	}
 
 	/**
-	 * Inicializa los atributos de la clase.
+	 * Inicializa y configura los componentes de la interfaz gráfica.
 	 */
 	public void initComponents()
 	{
-		panel = new JPanel();
-		lblSelectDisco = new JLabel();
-		lblSelectCancion = new JLabel();
-		btnAdd = new JButton();
-		cboxDisco = new JComboBox<>();
-		cboxCancion = new JComboBox<>();
-	}
+		panel = new AniadirCancionDisco(multimediaController, cancionController);
+		panel.setPreferredSize(new Dimension(350, 450));
 
-	/**
-	 * Configura los componentes de la interfaz gráfica.
-	 */
-	public void configComponents()
-	{
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setBounds(Constantes.POSITION_X_WINDOWS, Constantes.POSITION_Y_WINDOWS, Constantes.BOUNDS_WIDTH_WINDOWS, Constantes.BOUNDS_HEIGHT_WINDOWS);
-		setTitle("Agregar canciones a disco");
-
-		lblSelectDisco.setText("Selecciona un disco: ");
-		lblSelectCancion.setText("Selecciona una cancion: ");
-
-		cboxDisco = new JComboBox(multimediaController.obtenerDiscosTitulo());
-		cboxCancion = new JComboBox(cancionController.obtenerCancionesNombre());
-
-		btnAdd.setText("Añadir");
-		btnAdd.addActionListener(this);
-
-		panel.add(lblSelectDisco);
-		panel.add(cboxDisco);
-		panel.add(lblSelectCancion);
-		panel.add(cboxCancion);
-		panel.add(btnAdd);
-
+		setLayout(new FlowLayout());
 		add(panel);
+		pack();
+
+		getContentPane().setBackground(Color.orange);
+		setBounds(Constantes.POSITION_X_WINDOWS, Constantes.POSITION_Y_WINDOWS,
+				Constantes.BOUNDS_WIDTH_WINDOWS, Constantes.BOUNDS_HEIGHT_WINDOWS
+		);
+		setTitle("Añadir canción a disco");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
-	/**
-	 * Actualiza los ComboBox de disco y canción.
-	 */
 	public void refreshComboBox()
 	{
-		DefaultComboBoxModel<String> modelDisco = new DefaultComboBoxModel<>(multimediaController.obtenerDiscosTitulo());
-		cboxDisco.setModel(modelDisco);
-		DefaultComboBoxModel<String> modelCancion = new DefaultComboBoxModel<>(cancionController.obtenerCancionesNombre());
-		cboxCancion.setModel(modelCancion);
+		panel.refreshComboBox();
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e)
+	class AniadirCancionDisco extends JPanel implements ActionListener
 	{
-		try
+		/**
+		 * Declaración de controllers
+		 */
+		MultimediaController multimediaController;
+		CancionController cancionController;
+		/**
+		 * Declaración de atributos
+		 */
+		private JLabel lblSelectDisco;
+		private JLabel lblSelectCancion;
+		private JButton btnAdd;
+		private JComboBox cboxDisco;
+		private JComboBox cboxCancion;
+		private final ArrayList<Cancion> listCanciones;
+		private ArrayList<Disco> listDiscos;
+
+		/**
+		 * Inicializa una instancia de la clase AddCancionDiscoDesign.
+		 *
+		 * @param mc el controlador de multimedia
+		 * @param cc el controlador de canciones
+		 */
+		public AniadirCancionDisco(MultimediaController mc, CancionController cc)
 		{
-			multimediaController.aniadirCancionToDisco(
-					cancionController.filtroCancionPorNombre(cboxCancion.getSelectedItem().toString()),
-					multimediaController.filtroDiscoPorTitulo(cboxDisco.getSelectedItem().toString())
-			);
-			Cancion cancion = cancionController.filtroCancionPorNombre(cboxCancion.getSelectedItem().toString());
-			cancionController.darBajaCancion(cancion.getNombre(), cancion.getDuracion());
-			JOptionPane.showMessageDialog(null, "Cancion agregada al disco correctamente");
+			multimediaController = mc;
+			cancionController = cc;
+			listCanciones = cancionController.returnCancion();
+
+			initComponents();
+			configComponents();
+		}
+
+		/**
+		 * Inicializa los atributos de la clase.
+		 */
+		public void initComponents()
+		{
+			lblSelectDisco = new JLabel();
+			lblSelectCancion = new JLabel();
+			btnAdd = new JButton();
+			cboxDisco = new JComboBox<>();
+			cboxCancion = new JComboBox<>();
+		}
+
+		/**
+		 * Configura los componentes de la interfaz gráfica.
+		 */
+		public void configComponents()
+		{
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			setBounds(Constantes.POSITION_X_WINDOWS, Constantes.POSITION_Y_WINDOWS, Constantes.BOUNDS_WIDTH_WINDOWS, Constantes.BOUNDS_HEIGHT_WINDOWS);
+			setTitle("Agregar canciones a disco");
+
+			lblSelectDisco.setText("Selecciona un disco: ");
+			lblSelectCancion.setText("Selecciona una cancion: ");
+
+			cboxDisco = new JComboBox(multimediaController.obtenerDiscosTitulo());
+			cboxCancion = new JComboBox(cancionController.obtenerCancionesNombre());
+
+			btnAdd.setText("Añadir");
+			btnAdd.addActionListener(this);
+
+			setLayout(new GridLayout(3, 2));
+			add(lblSelectDisco);
+			add(cboxDisco);
+			add(lblSelectCancion);
+			add(cboxCancion);
+			add(new JPanel());
+			add(btnAdd);
+		}
+
+		/**
+		 * Actualiza los ComboBox de disco y canción.
+		 */
+		public void refreshComboBox()
+		{
+			DefaultComboBoxModel<String> modelDisco = new DefaultComboBoxModel<>(multimediaController.obtenerDiscosTitulo());
+			cboxDisco.setModel(modelDisco);
 			DefaultComboBoxModel<String> modelCancion = new DefaultComboBoxModel<>(cancionController.obtenerCancionesNombre());
 			cboxCancion.setModel(modelCancion);
 		}
-		catch (Exception exception)
+
+		@Override
+		public void actionPerformed(ActionEvent e)
 		{
-			JOptionPane.showMessageDialog(null, "Fallo al añadir cancion al disco",
-					"ERROR", JOptionPane.ERROR_MESSAGE
-			);
+			try
+			{
+				multimediaController.aniadirCancionToDisco(
+						cancionController.filtroCancionPorNombre(cboxCancion.getSelectedItem().toString()),
+						multimediaController.filtroDiscoPorTitulo(cboxDisco.getSelectedItem().toString())
+				);
+				Cancion cancion = cancionController.filtroCancionPorNombre(cboxCancion.getSelectedItem().toString());
+				cancionController.darBajaCancion(cancion.getNombre(), cancion.getDuracion());
+				JOptionPane.showMessageDialog(null, "Cancion agregada al disco correctamente");
+				DefaultComboBoxModel<String> modelCancion = new DefaultComboBoxModel<>(cancionController.obtenerCancionesNombre());
+				cboxCancion.setModel(modelCancion);
+			}
+			catch (Exception exception)
+			{
+				JOptionPane.showMessageDialog(null, "Fallo al añadir cancion al disco",
+						"ERROR", JOptionPane.ERROR_MESSAGE
+				);
+			}
 		}
 	}
 }
